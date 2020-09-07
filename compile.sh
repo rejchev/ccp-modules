b953fb7ee1fc08f5f9242fc76cea34fe0cd7d5e8
IIEnt <undertowna@gmail.com>
2020-09-07 23:52:02 +0500

    Travis CI

--- compile.sh
+++ compile.sh
@@ -1,13 +1,8 @@
 #!/bin/bash
 
-echo $1
 cd $1
 files=(*.sp)
 
 for file in "${files[@]}"; do
-    # if [[ -e "${files[$file]}" ]]; then
-    #     continue
-    # fi
-    echo $file
     ./spcomp $file -E -v0 
 done
