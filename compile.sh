155f56d3eb3862a03389a4b857d000c0771614c7
IIEnt <undertowna@gmail.com>
2020-09-07 23:41:50 +0500

    compile

--- compile.sh
+++ compile.sh
@@ -4,9 +4,9 @@ cd $1
 files=(*.sp)
 
 for file in "${files[@]}"; do
-    if [[ -e ${files[$file]} ]]; then
-        continue
-    fi
+    # if [[ -e "${files[$file]}" ]]; then
+    #     continue
+    # fi
 
     ./spcomp -E -v0 "${files[$file]}"
 done
